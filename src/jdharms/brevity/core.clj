e3576799099efb46f3b9eca3a5ba69decb09eff5
Daniel Harms <jdharms@gmail.com>
2026-06-22 16:42:21 -0400

    Basic support for :leave

--- src/jdharms/brevity/core.clj
+++ src/jdharms/brevity/core.clj
@@ -5,9 +5,17 @@
   {:event event
    :system system})
 
-(defn execute [system interceptors event]
-  (let [ctx (make-context event system)]
-    (reduce (fn [ctx i] ((:enter i) ctx)) ctx interceptors)))
+(defn execute [system chain event]
+  (let [initial-ctx (make-context event system)
+        initial-on-leave []
+        after-enters (reduce (fn [{:keys [ctx on-leave]} i] 
+                               (let [new-ctx ((:enter i) ctx)
+                                     new-on-leave (if (:leave i) (conj on-leave i) on-leave)]
+                                 {:ctx new-ctx :on-leave new-on-leave})) {:ctx initial-ctx :on-leave initial-on-leave} chain)
+        after-leaves (reduce (fn [ctx i] ((:leave i) ctx)) (:ctx after-enters) (reverse (:on-leave after-enters)))]
+    
+    after-leaves))
+
 
 (comment
   (defn event [type]
@@ -23,7 +31,7 @@
 
   (def adds-a-key (interceptor "adds-a-key" (fn [ctx] (assoc ctx :my-new-key "here's a value"))))
 
-  (let [interceptors [adds-a-key]]
-    (execute system interceptors (event :test-event)))
+  (let [chain [adds-a-key]]
+    (execute system chain (event :test-event)))
   
   (event :chat-message))
--- test/jdharms/brevity/core_test.clj
+++ test/jdharms/brevity/core_test.clj
@@ -20,4 +20,37 @@
                                    (swap! recorder conj n)
                                    ctx)})
           _ (sut/execute {} [(make-i 1) (make-i 2) (make-i 3)] {:type :test-event})]
-      (is (= [1 2 3] @recorder)))))
+      (is (= [1 2 3] @recorder))))
+  
+  (testing "single interceptor's leave runs after enter"
+    (let [recorder (atom [])
+          record (fn [phase] (fn [ctx] (swap! recorder conj phase) ctx))
+          i {:name "i"
+             :enter (record :enter)
+             :leave (record :leave)}
+          _ (sut/execute {} [i] {:type :test-event})]
+      (is (= [:enter :leave] @recorder))))
+  
+  (testing "leave functions run in reverse order"
+    (let [recorder (atom [])
+          record (fn [phase] (fn [ctx] (swap! recorder conj phase) ctx))
+          a {:name "a"
+             :enter (record :enter-a)
+             :leave (record :leave-a)}
+          b {:name "b"
+             :enter (record :enter-b)
+             :leave (record :leave-b)}
+          _ (sut/execute {} [a b] {:type :test-event})]
+      (is (= [:enter-a :enter-b :leave-b :leave-a] @recorder))))
+  
+  (testing "execute handles interceptors without :leave properly"
+    (let [recorder (atom [])
+          record (fn [phase] (fn [ctx] (swap! recorder conj phase) ctx))
+          a {:name "a"
+              :enter (record :enter-a)
+              :leave (record :leave-a)}
+          b {:name "b"
+              :enter (record :enter-b)
+              :leave nil}
+          _ (sut/execute {} [a b] {:type :test-event})]
+      (is (= [:enter-a :enter-b :leave-a] @recorder)))))
